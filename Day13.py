ef595bd232801b1be16215aba6e8963eaebb0a87
Christopher Tay <tay.christopher@gmail.com>
2019-12-15 10:33:24 +0800

    Day 13 part 2

--- Day13.py
+++ Day13.py
@@ -18,3 +18,23 @@ def draw_tiles(code):
 
 p1 = len([x for x in draw_tiles(list(code)).values() if x==BLOCK])
 print(f"Part 1: {p1}")
+
+def break_blocks():
+    input, paddle_x = [], 0
+    c = list(code)
+    c[0] = 2
+    prog = Intcode(c, input)
+
+    try:
+        while True:
+            x, y, z = next(prog), next(prog), next(prog)
+            if (x,y)==(-1,0):
+                print(z, end=' ')
+            paddle_x =  x if z==PADDLE else paddle_x
+            if z==BALL:
+                input.append((x>paddle_x)-(x<paddle_x))
+    except StopIteration:
+        print("End of gameplay")
+
+print("Part 2:")
+break_blocks()
