654450d669c248ce054eceeb4b25d080eda2007b
Christpoher Tay <christopher.tay@vidimensions.com>
2019-12-18 15:44:45 +0800

    Day 17 cleanup

--- Day17.py
+++ Day17.py
@@ -7,29 +7,22 @@ code = [int(x) for x in open("Day17.txt").readline().strip().split(",")]+[0]*100
 
 SF, SP, EOL = 35, 46, 10
 
-def getmap(code, input=[]):
+def getmap2(code, input=[]):
     prog = Intcode(code, input)
-    m, r, c = defaultdict(int), 0, 0
-    NR, NC = 0, 0
-    try:
-        while True:
-            e = next(prog)
-            if e==EOL:
-                r, c = r+1, 0
-            else:
-                m[(r,c)] = e
-                c += 1
-            NR, NC = max(NR, r), max(NC, c)
-    except StopIteration:
-        print("Program terminate")
-    return m, NR, NC
+    arr = "".join([chr(x) for x in prog]).split(chr(10))
+    m_str = [e for e in  arr if len(e)>1]
+    d, NR, NC = defaultdict(int), len(m_str), len(m_str[0])
+    m_ord = [ord(x) for x in "".join(m_str)]
+    for coord, e in zip( [(r,c) for r in range(NR) for c in range(NC)], m_ord):
+        d[coord] = e
+    return d, NR, NC, ord(arr[-1]) if len(arr[-1])==1 else None
 
 def alignment(m, nr, nc):
     is_inter = lambda r, c: m[(r,c)]==SF and m[(r+1,c)]==SF and m[(r-1,c)]==SF and m[(r,c+1)]==SF and m[(r,c-1)]==SF
     s =[r*c for r in range(1, nr-1) for c in range(1, nc-1) if is_inter(r,c)]
     return sum(s)
 
-ship_map, NR, NC = getmap(list(code))
+ship_map, NR, NC, _ = getmap2(list(code))
 
 def draw_map():
     for r in range(NR):
@@ -44,28 +37,21 @@ L = [(0,-1), (0,1), (-1,0), (1,0)]
 R = [(0,1), (0,-1), (1,0), (-1,0)]
 start_pos = [p for p, t in ship_map.items() if t==ord('^')][0]
 
-def trace(pos):
-    path, Dir, vec, count = [], N, (-1,0), 0
-    is_SF_rel = lambda x, y: ship_map[(pos[0]+x, pos[1]+y)] == SF
-    while True:
-        if is_SF_rel(*vec):
-            pos = (pos[0]+vec[0], pos[1]+vec[1])
-            count += 1
-        elif is_SF_rel(*L[Dir]):
-            vec = L[Dir]
-            Dir = [W, E, N, S][Dir]
-            path += [str(count), 'L']
-            count = 0
-        elif is_SF_rel(*R[Dir]):
-            vec = R[Dir]
-            Dir = [E, W, S, N][Dir]
-            path += [str(count), 'R']
-            count = 0
-        else:
-            path += [str(count)]
-            return path[1:]
+is_SF_rel = lambda p, v: ship_map[(p[0]+v[0], p[1]+v[1])] == SF
 
-path = trace(start_pos)
+def trace(pos, vec, dir, count):
+    if is_SF_rel(pos, vec):
+        return trace2((pos[0]+vec[0], pos[1]+vec[1]), vec, dir, count+1)
+    elif is_SF_rel(pos, L[dir]):
+        return [str(count), 'L'] + trace2(pos, L[dir], [W,E,N,S][dir], 0)
+    elif is_SF_rel(pos, R[dir]):
+        return [str(count), 'R'] + trace2(pos, R[dir], [E,W,S,N][dir], 0)
+    else:
+        return [str(count)]
+
+path = trace(start_pos, (-1,0), N, 0)
+if path[0]=='0':
+    path = path[1:]
 print(f"Full path: {path}")
 
 p2s = lambda p: "".join([str(x) for x in p])
@@ -94,7 +80,7 @@ def dust():
     input = p2inp(routine)+p2inp(A)+p2inp(B)+p2inp(C)+[N, EOL]
     c = list(code)
     c[0] = 2
-    m, nr, nc = getmap(c, input)
-    return m[(nr,0)]
+    m, nr, nc, d = getmap2(c, input)
+    return d
 
 print(f"Part 2: {dust()}")
