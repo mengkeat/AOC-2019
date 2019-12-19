4e0c26f275e18860b4169d31b3cf4a1c22e8f2b6
Christpoher Tay <christopher.tay@vidimensions.com>
2019-12-19 09:39:17 +0800

    Day 18 another checkpt

--- Day18.py
+++ Day18.py
@@ -1,5 +1,6 @@
 from functools import *
 from itertools import *
+import heapq
 
 m = [x.strip() for x in open("Day18-test2.txt").readlines()]
 NR, NC = len(m), len(m[0])
@@ -8,12 +9,12 @@ DOORS = [x.upper() for x in KEYS]
 ALL_NODES = KEYS + DOORS + ['@']
 DIR = [-1, 1, -1j, 1j] # N,S,E,W
 
-pos_map = dict([(m[r][c] ,r+c*1j) for r in range(NR) for c in range(NC) if m[r][c] in ALL_NODES])
+POS_MAP = dict([(m[r][c] ,r+c*1j) for r in range(NR) for c in range(NC) if m[r][c] in ALL_NODES])
 get_map = lambda p: m[int(p.real)][int(p.imag)]
 
 @lru_cache(maxsize=None)
 def neighbours(node):
-    visited, s = set([pos_map[node]]), [(pos_map[node], 0)]
+    visited, s = set([POS_MAP[node]]), [(POS_MAP[node], 0)]
     neigh = []
     while len(s):
         p, d = s.pop()
@@ -27,9 +28,9 @@ def neighbours(node):
     return dict(neigh)
 
 from math import inf
-shortest = dict( [(n2, dict([(n1, inf) for n1 in pos_map.keys()])) for n2 in pos_map.keys()] )
-req_keys = dict( [(n2, dict([(n1, set()) for n1 in pos_map.keys()])) for n2 in pos_map.keys()])
-for k in pos_map.keys():
+shortest = dict( [(n2, dict([(n1, inf) for n1 in POS_MAP.keys()])) for n2 in POS_MAP.keys()] )
+req_keys = dict( [(n2, dict([(n1, set()) for n1 in POS_MAP.keys()])) for n2 in POS_MAP.keys()])
+for k in POS_MAP.keys():
     shortest[k][k] = 0
     for n_k in neighbours(k):
         shortest[k][n_k] = neighbours(k)[n_k]
@@ -37,7 +38,7 @@ for k in pos_map.keys():
             req_keys[k][n_k].add(n_k.lower())
         if k in DOORS:
             req_keys[k][n_k].add(k.lower())
-for k,i,j in product(pos_map.keys(), repeat=3):
+for k,i,j in product(POS_MAP.keys(), repeat=3):
     if shortest[i][j] > shortest[i][k]+shortest[k][j]:
         shortest[i][j] = shortest[i][k]+shortest[k][j]
         req_keys[i][j] = req_keys[i][j].union(req_keys[i][k])
@@ -45,3 +46,7 @@ for k,i,j in product(pos_map.keys(), repeat=3):
 
 print(shortest)
 print(req_keys)
+
+def can_access(start, end, keys):
+    keys_required = req_keys[start][end]
+    return all([k in keys_required for k in keys])
