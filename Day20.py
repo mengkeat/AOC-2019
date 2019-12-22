03a4b05bb33f88bc0a83e80c3f8bda55f6c226a2
christopher <tay.christopher@gmail.com>
2019-12-22 10:22:19 +0800

    Day 20 part 1

--- Day20.py
+++ Day20.py
@@ -1,19 +1,33 @@
 from collections import defaultdict
 from functools import *
 from itertools import *
+from heapq import *
 
 rawdat = [x.strip('\n') for x in open("Day20.txt").readlines()]
 labels = defaultdict(list)
 def get(r1, c1, r2, c2, r, c):
     raw = rawdat[r+r1][c+c1]+rawdat[r+r2][c+c2]
     return raw if raw.isalpha() else None
-NR, NC = len(rawdat), len(rawdat[0])
-print(f"Rows: {NR} Cols: {NC}")
 
 D = [(-2,0,-1,0), (1,0,2,0), (0,-2,0,-1), (0,1,0,2)]
-for r in range(2, NR-2):
-    for c in range(2, NC-2):
+for r in range(2, len(rawdat)-2):
+    for c in range(2, len(rawdat[0])-2):
         lbl = reduce(lambda x, y: x or y, [get(*d, r, c) for d in D], None)
         if rawdat[r][c] == '.' and lbl:
             labels[lbl].append((r,c))
-# [print(k,v) for k,v in labels.items()]
+JMP = dict( [v for k,v in labels.items() if k not in ('AA', 'ZZ')] + \
+            [v[::-1] for k,v in labels.items() if k not in ('AA', 'ZZ')] )
+
+D = [(0,1), (0,-1), (1,0), (-1,0)]
+def get_steps(start, end):
+    V, Q = set(), [(0, start)]
+    while Q:
+        d, coord = heappop(Q)
+        if coord == end: return d
+        V.add(coord)
+        cand = [(coord[0]+dx, coord[1]+dy) for dx,dy in D] + ([JMP[coord]] if coord in JMP else [])
+        for p in cand:
+            if rawdat[p[0]][p[1]]=='.' and p not in V:
+                heappush(Q, (d+1, p))
+
+print(f"Part 1: {get_steps(labels['AA'][0], labels['ZZ'][0])}")
